637ac82071541418426a15f2d2a4b39e2df0795e
Kevin James <kevinjamesus86@gmail.com>
2017-07-28 15:18:02 -0700

    make the SiteHeader a little more fun
    feature: fun SiteHeader styles
    
    Who doesn't love a good gragient? There is now a gradient.

--- src/SiteHeader.js
+++ src/SiteHeader.js
@@ -5,37 +5,39 @@ import './SiteHeader.css';
 export default class SiteHeader extends Component {
   render() {
     return (
-      <header className="site-header">
-        <h1 className="site-header__logo">MY READS</h1>
-        <nav className="site-links">
-          <NavLink
-            exact
-            to="/"
-            className="site-links__link">
-            all
-          </NavLink>
-          <span className="site-links__link-divider"></span>
-          <NavLink
-            exact
-            to="/reading"
-            className="site-links__link">
-            reading
-          </NavLink>
-          <span className="site-links__link-divider"></span>
-          <NavLink
-            exact
-            to="/will-read"
-            className="site-links__link">
-            will read
-          </NavLink>
-          <span className="site-links__link-divider"></span>
-          <NavLink
-            exact
-            to="/read"
-            className="site-links__link">
-            read
-          </NavLink>
-        </nav>
+      <header className="header">
+        <div className="header-inner">
+          <h1 className="header__logo">my reads</h1>
+          <nav className="site-links">
+            <NavLink
+              exact
+              to="/"
+              className="site-links__link">
+              all
+            </NavLink>
+            <span className="site-links__link-divider"></span>
+            <NavLink
+              exact
+              to="/reading"
+              className="site-links__link">
+              reading
+            </NavLink>
+            <span className="site-links__link-divider"></span>
+            <NavLink
+              exact
+              to="/will-read"
+              className="site-links__link">
+              will read
+            </NavLink>
+            <span className="site-links__link-divider"></span>
+            <NavLink
+              exact
+              to="/read"
+              className="site-links__link">
+              read
+            </NavLink>
+          </nav>
+        </div>
       </header>
     );
   }
