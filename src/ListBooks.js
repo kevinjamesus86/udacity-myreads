994401c5ac784070c0c1e7612658297eca704b4d
Kevin James <kevinjamesus86@gmail.com>
2017-07-28 15:57:06 -0700

    feat: ListBooks
    feat: ListBooks
    
    The ListBooks component is responsible for rendering an unordered list of books. This is reusable across shelves and search results :+1:

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import './ListBooks.css';
+import Book from './Book';
 
 export default class ListBooks extends Component {
+  static propTypes = {
+    books: PropTypes.array.isRequired,
+  };
   render() {
+    const { books } = this.props;
     return (
-      <div>ListBooks</div>
+      <ul className="list-books">
+        {books.map(book =>
+          <li key={book.id} className="list-books__item">
+            <Book book={book} />
+          </li>
+        )}
+      </ul>
     );
   }
 }
